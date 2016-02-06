11ecb4577bd89169a0e64b5d91a125380e163065
Martin Cavoj <martin@panacom.sk>
2016-02-06 15:58:45 +0100

    Remember username to session storage

--- app/game/profile-service.js
+++ app/game/profile-service.js
@@ -1,18 +1,23 @@
 (function() {
   'use strict';
 
-  function ProfileService() {
+  function ProfileService($window) {
     var _userData = {};
 
     return {
       setUsername: function(username) {
         _userData.username = username;
+        $window.sessionStorage.username = username;
       },
       getUsername: function() {
+        if (!_userData.username && $window.sessionStorage.username) {
+          _userData.username = $window.sessionStorage.username;
+        }
         return _userData.username;
       }
     };
   }
+  ProfileService.$inject = ['$window'];
 
   angular.module('wordgame.game')
   .factory('Profile', ProfileService);
--- app/game/profile-service_test.js
+++ app/game/profile-service_test.js
@@ -10,9 +10,23 @@ describe('wordgame.game module', function() {
   }));
 
   describe('profile service', function() {
+    beforeEach(inject(function($window) {
+      delete $window.sessionStorage.username;
+    }));
     it('should remember username', function() {
       Profile.setUsername('test1');
       expect(Profile.getUsername()).toEqual('test1');
     });
+
+    it('should remember username in session storage', inject(function($window) {
+      Profile.setUsername('test1');
+      expect($window.sessionStorage.username).toBe('test1');
+    }));
+
+    it('should load username from session storage if it is present there', inject(function($window) {
+      expect(Profile.getUsername()).toBeUndefined();
+      $window.sessionStorage.username = 'test1';
+      expect(Profile.getUsername()).toBe('test1');
+    }));
   });
 });
